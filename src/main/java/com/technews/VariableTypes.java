99f701da4f2a6f3769c24209b8d59c7bf6e3350f
mercspring <68706508+mercspring@users.noreply.github.com>
2021-05-10 14:08:33 -0700

    helloworld and practice vars

---
+++ src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,14 @@
+package com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args){
+        String name = "aries";
+        int age = 6 + 7;
+        double price = 8.32414;
+        boolean no = false;
+        System.out.println(!no);
+        System.out.println(price + name);
+        System.out.println(age);
+        System.out.println(price + age);
+    }
+}
